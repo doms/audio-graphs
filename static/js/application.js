006ae147799ff1c5039ceab4a23edd7e99c95e94
Dominic <domyoung424@gmail.com>
2018-12-25 04:14:24 -0600

    :star: html/css/js

---
+++ static/js/application.js
@@ -0,0 +1,179 @@
+;(() => {
+  var ctx
+
+  function resetCanvas() {
+    const featureChartContainer = document.getElementById(
+      "features-chart-container"
+    )
+
+    const featureChart = document.getElementById("features-chart")
+    featureChartContainer.removeChild(featureChart)
+
+    let canvas = document.createElement("canvas")
+    canvas.setAttribute("id", "features-chart")
+    featureChartContainer.appendChild(canvas)
+
+    ctx = document.getElementById("features-chart")
+  }
+
+  function getFeatures(id) {
+    resetCanvas()
+
+    // set search query with id
+    let query = `/features?id=${id}`
+
+    fetch(query)
+      .then(response => response.json())
+      .then(data => {
+        // [{...}]
+        data = data[0]
+
+        const labels = []
+        const values = []
+        const features = [
+          "acousticness",
+          "danceability",
+          "energy",
+          "instrumentalness",
+          "liveness",
+          "loudness",
+          "mode",
+          "speechiness",
+          "tempo",
+          "valence"
+        ]
+
+        for (let feature in data) {
+          if (data.hasOwnProperty(feature) && features.indexOf(feature) !== -1) {
+            if (data[feature] <= 1 && data[feature] > 0) {
+              labels.push(feature)
+              values.push(data[feature])
+            }
+          }
+        }
+
+        // chart options
+        const options = {
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  max: 1
+                }
+              }
+            ]
+          }
+        }
+
+        // chart data (layout)
+        const layout = {
+          labels: labels,
+          datasets: [
+            {
+              data: values,
+              backgroundColor: [
+                "rgba(30,215,96, 0.9)",
+                "rgba(245,115,160, 0.9)",
+                "rgba(80,155,245, 0.9)",
+                "rgba(255,100,55, 0.9)",
+                "rgba(180,155,200, 0.9)",
+                "rgba(250,230,45, 0.9)",
+                "rgba(0,100,80, 0.9)",
+                "rgba(175,40,150, 0.9)",
+                "rgba(30,50,100, 0.9)"
+              ],
+              borderColor: [
+                "rgba(30,215,96, 1)",
+                "rgba(245,115,160, 1)",
+                "rgba(80,155,245, 1)",
+                "rgba(255,100,55, 1)",
+                "rgba(180,155,200, 1)",
+                "rgba(250,230,45, 1)",
+                "rgba(0,100,80, 1)",
+                "rgba(175,40,150, 1)",
+                "rgba(30,50,100, 1)"
+              ],
+              borderWidth: 1
+            }
+          ]
+        }
+
+        var myChart = new Chart(ctx, {
+          type: "bar",
+          data: layout,
+          options: options
+        })
+      })
+  }
+
+  let trackID = ""
+  let searchQuery = ""
+  let resultIDs = []
+  let listenerAttached = false
+
+  const tracks = document.getElementById("tracks")
+
+  document.querySelector("form").onsubmit = event => {
+    event.preventDefault()
+
+    searchQuery = `/search?query=${document.querySelector("input").value}`
+
+    // search for tracks
+    fetch(searchQuery)
+      .then(response => response.json())
+      .then(json => {
+        if (listenerAttached) {
+          tracks.removeEventListener("click")
+          listenerAttached = false
+        }
+
+        // clear previous search results
+        tracks.textContent = ""
+
+        json.forEach(track => {
+          resultIDs.push(track.id)
+
+          let el = document.createElement("button")
+          el.className = "text-salmon track"
+          el.value = track.id
+          el.textContent = `${track.name}   |   ${track.artists[0].name}`
+
+          el.onclick = () => {
+            getFeatures(track.id)
+          }
+          tracks.appendChild(el)
+        })
+
+        tracks.addEventListener("click", function(event) {
+          const button = event.target.closest(".track")
+          if (!button) return
+
+          getFeatures(button.value)
+        })
+
+        listenerAttached = true
+      })
+  }
+
+  // https://koddsson.com/posts/emoji-favicon/
+  const favicon = document.querySelector("link[rel=icon]")
+  if (favicon) {
+    const emoji = favicon.getAttribute("data-emoji")
+
+    if (emoji) {
+      const canvas = document.createElement("canvas")
+      canvas.height = 64
+      canvas.width = 64
+
+      const ctx = canvas.getContext("2d")
+      ctx.font = "64px serif"
+      ctx.fillText(emoji, 0, 64)
+
+      favicon.href = canvas.toDataURL()
+    }
+  }
+})()
