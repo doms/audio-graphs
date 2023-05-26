3979ee2c919a59ee2f200e9afd95588e0e4e1a8f
Dominic <8808097+doms@users.noreply.github.com>
2023-05-25 22:18:45 -0700

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import React from 'react'
 
@@ -30,52 +29,63 @@ export default function Home() {
       {} as SpotifyApi.AudioFeaturesObject
     )
 
+  const [trackListBorder, setTrackListBorder] = useState({ border: 'none' })
+
+  useEffect(() => {
+    const nextBorderStyles =
+      tracks.length > 0 ? { border: '1px solid #000' } : { border: 'none' }
+    setTrackListBorder(nextBorderStyles)
+  }, [tracks])
+
   return (
     <>
-      <Head>
-        <title>Audio Graphs 📊</title>
-        <meta
-          name='description'
-          content='Allows you to view the audio features of a song in a simple way'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <header>
-        <input
-          type='text'
-          maxLength={50}
-          placeholder='Track Name...'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button
-          className=''
-          onClick={async () => {
-            try {
-              const response = await fetch(`/api/get-tracks?q=${searchTerm}`)
-              const tracks = await response.json()
-              setTracks(tracks.tracks.items)
-            } catch (error) {
-              console.log('[get-tracks] error', error)
-            }
-          }}
-        >
-          Search
-        </button>
-        <br />
+      <header className={styles.header}>
+        <h1>
+          Audio Graphs{' '}
+          <span className={styles.author}>
+            by{' '}
+            <a href='//github.com/doms' target='_blank' rel='noreferrer'>
+              @doms
+            </a>
+          </span>
+        </h1>
       </header>
 
-      <main>
-        <div className='tracks'>
+      <main className={styles.main}>
+        <div className={styles.inputWrapper}>
+          <input
+            type='text'
+            maxLength={50}
+            placeholder='Search for a song...'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button
+            onClick={async () => {
+              try {
+                const response = await fetch(`/api/get-tracks?q=${searchTerm}`)
+                const tracks = await response.json()
+                setTracks(tracks.tracks.items)
+              } catch (error) {
+                console.log('[get-tracks] error', error)
+              }
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <br />
+        <div className={styles.tracks} style={trackListBorder}>
           {tracks.map((track) => (
             <div
+              className={styles.track}
               key={track.id}
               onClick={async () => {
                 try {
                   const response = await fetch(
                     `/api/get-audio-features?id=${track.id}`
                   )
+
                   const feature = await response.json()
                   setTrackFeatures(feature)
                 } catch (error) {
@@ -83,85 +93,85 @@ export default function Home() {
                 }
               }}
             >
-              {track.name}
+              {track.artists.map(({ name }) => name).join(', ')} -- {track.name}
             </div>
           ))}
         </div>
-        <section className='features'></section>
+
+        {Object.keys(trackFeatures).length ? (
+          <Bar
+            data={layout(assembleGraph(trackFeatures))}
+            options={{ plugins: { legend: { display: false } } }}
+          />
+        ) : null}
       </main>
     </>
   )
 }
 
-// chart options
-const options = {
-  legend: {
-    display: false,
-  },
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true,
-          max: 1,
-        },
-      },
-    ],
-  },
+/*
+
+*/
+
+function assembleGraph(data: SpotifyApi.AudioFeaturesObject) {
+  const labels = []
+  const values = []
+  const features = [
+    'acousticness',
+    'danceability',
+    'energy',
+    'instrumentalness',
+    'liveness',
+    'loudness',
+    'mode',
+    'speechiness',
+    'tempo',
+    'valence',
+  ]
+
+  for (const [feature, value] of Object.entries(data)) {
+    if (features.indexOf(feature) !== -1) {
+      if (value <= 1 && value > 0) {
+        labels.push(feature)
+        values.push(value)
+      }
+    }
+  }
+
+  return { labels, values }
 }
 
-// const labels: string[] = []
-// const values = []
-// const features = [
-//   'acousticness',
-//   'danceability',
-//   'energy',
-//   'instrumentalness',
-//   'liveness',
-//   'loudness',
-//   'mode',
-//   'speechiness',
-//   'tempo',
-//   'valence',
-// ]
-
-// for (let feature in data) {
-//   if (data.hasOwnProperty(feature) && features.indexOf(feature) !== -1) {
-//     if (data[feature] <= 1 && data[feature] > 0) {
-//       labels.push(feature)
-//       values.push(data[feature])
-//     }
-//   }
-// }
-
-// const layout = {
-//   labels: labels,
-//   datasets: [
-//     {
-//       data: values,
-//       backgroundColor: [
-//         'rgba(30,215,96, 0.9)',
-//         'rgba(245,115,160, 0.9)',
-//         'rgba(80,155,245, 0.9)',
-//         'rgba(255,100,55, 0.9)',
-//         'rgba(180,155,200, 0.9)',
-//         'rgba(250,230,45, 0.9)',
-//         'rgba(0,100,80, 0.9)',
-//         'rgba(175,40,150, 0.9)',
-//         'rgba(30,50,100, 0.9)',
-//       ],
-//       borderColor: [
-//         'rgba(30,215,96, 1)',
-//         'rgba(245,115,160, 1)',
-//         'rgba(80,155,245, 1)',
-//         'rgba(255,100,55, 1)',
-//         'rgba(180,155,200, 1)',
-//         'rgba(250,230,45, 1)',
-//         'rgba(0,100,80, 1)',
-//         'rgba(175,40,150, 1)',
-//         'rgba(30,50,100, 1)',
-//       ],
-//       borderWidth: 1,
-//     },
-//   ],
-// }
+const layout = (data: { labels: string[]; values: number[] }) => {
+  const { labels, values } = data
+  return {
+    labels,
+    datasets: [
+      {
+        data: values,
+        backgroundColor: [
+          'rgba(30,215,96, 0.9)',
+          'rgba(245,115,160, 0.9)',
+          'rgba(80,155,245, 0.9)',
+          'rgba(255,100,55, 0.9)',
+          'rgba(180,155,200, 0.9)',
+          'rgba(250,230,45, 0.9)',
+          'rgba(0,100,80, 0.9)',
+          'rgba(175,40,150, 0.9)',
+          'rgba(30,50,100, 0.9)',
+        ],
+        borderColor: [
+          'rgba(30,215,96, 1)',
+          'rgba(245,115,160, 1)',
+          'rgba(80,155,245, 1)',
+          'rgba(255,100,55, 1)',
+          'rgba(180,155,200, 1)',
+          'rgba(250,230,45, 1)',
+          'rgba(0,100,80, 1)',
+          'rgba(175,40,150, 1)',
+          'rgba(30,50,100, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }
+}
